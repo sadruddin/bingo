b98d6ecb93712575ccceaff6927481ec2463309a
Sadruddin Rejeb <sadrejeb@outlook.com>
2025-01-12 08:05:28 +0100

    wip

---
+++ static/js/952.d442f49a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbingo=self.webpackChunkbingo||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.d442f49a.chunk.js.map
